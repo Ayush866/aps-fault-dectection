5c861dc112ee9db9467cfd6cce3edeb500b0fbc6
Ayush866 <ayush786bisht@gmail.com>
2023-01-10 13:27:38 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requriments():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Ayush",
+    author_email="ayush786bisht@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requriments(),
+)
